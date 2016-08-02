2f185bb949aff58a011dd5e3cb72258b44c7d6a6
Roman Kříž <samnung@gmail.com>
2016-08-03 00:51:47 +0200

    [spec] fix failing spec, be_truthy is not working on travis

--- spec/sprockets_spec.rb
+++ spec/sprockets_spec.rb
@@ -30,7 +30,7 @@ describe 'Sprockets setup' do
 
   it 'configure the debug and compress flags' do
     Stylus.setup(env, debug: true, compress: true)
-    expect(Stylus.debug).to be_truthy
-    expect(Stylus.compress).to be_truthy
+    expect(Stylus.debug).to eq true
+    expect(Stylus.compress).to eq true
   end
 end
